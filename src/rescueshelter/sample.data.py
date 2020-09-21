aed1ddcd0ca2af5a74d3a8045db6830c009cda56
Karega K. Scott <kkscott@outlook.com>
2020-09-21 15:52:09 -0500

    Structure changes

--- src/rescueshelter/sample.data.py
+++ src/rescueshelter/sample.data.py
@@ -13,6 +13,52 @@ from multiprocessing import Pool
 from multiprocessing.pool import AsyncResult
 from typing import Iterable, Any
 
+word_size_min = 5
+word_size_max = 10
+wordSize = random.randint(word_size_min, word_size_max)
+
+word_generator = random.sample #Saves the sample([],k=0) function for use later
+wordTemplate =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+animalImageIconType_choice = random.choice
+animalImageIconTypes = ['deskpro', 'docker', 'earlybirds', 'drupal', 'firefox',
+    'github', 'gitlab', 'grunt', 'linux', 'napster', 'phoenix framework', 'qq',
+    'reddit alien', 'snapchat ghost', 'sticker mule', 'twitter', 'tripadvisor',
+    'vaadin', 'themeisle', 'github alternate']
+
+animalCategoryType_choice = random.choice
+animalCategoryTypes = ['fishes', 'amphibians', 'reptiles', 'birds', 'mammals', 'invertebrates']
+
+endangeredTypes_choice = random.choice #Saves the choice([]) function for use later
+endangeredTypes = [ True, False]
+
+size_min = 100
+size_max = 500
+population_generator = random.randint #Saves the randint(min,max) function for use later
+
+description = '''Lorem ipsum dōlor sit ǽmet, reqūe tation constiÞuto vis eu, est ðōlor omnīum āntiopæm ei. 
+                Zril domīng cū eam, hās ið equīðem explīcærī voluptǽtum. Iusto regiōnē partiendo meǣ ne, vim 
+                cu ælii āltērum vōlutpāt, vis et aliquip trītæni. Dolor luptātum sapienÞem cu pēr, dico qūæs 
+                ðissentiǣs et eūm, vix ut.'''
+
+def populationData(populationToday):
+    delta = datetime.timedelta(days=-270)
+    utcnow = datetime.datetime.utcnow()
+    population = populationToday
+
+    data = []
+    for i in range(100) :
+        if i >= 1 : # entry
+            utcnow = utcnow+delta 
+            population = random.randint(100, 200)
+
+        data.append({
+            'population': population,
+            'created': utcnow
+        })
+
+    return data
+
 def bulkWriteListSizes(totalWrites: int = 1000000) : 
     CHUCK_SIZE = 1024 # History: Bytes in 1 Kilobyte
 
@@ -29,31 +75,6 @@ def bulkWriteListSizes(totalWrites: int = 1000000) :
     if sum(results) == totalWrites : return results
 
     return [0]
-
-def startMulticoreProcessFor(function, args: Iterable[Any], message  = 'Multiple core processor function evaluation') :
-    print(message)
-    starttime = datetime.datetime.now()
-
-    with Pool(processes=4) as pool :         
-        # launching multiple evaluations asynchronously *may* use more processes
-        asyncResults = [pool.apply_async(func=function, args=args) for i in range(4)]
-        
-        while True :
-            for asyncResult in asyncResults :  asyncResult.wait(1)
-
-            # Create a list of asyncResults ready state values
-            if False in [asyncResult.ready() for asyncResult in asyncResults] : continue # One function execution is not complete
-            else : break
-
-    endtime = datetime.datetime.now()    
-    print(f'Duration: {endtime-starttime}')
-
-word_size_min = 5
-word_size_max = 10
-wordSize = random.randint(word_size_min, word_size_max)
-
-word_generator = random.sample #Saves the sample([],k=0) function for use later
-wordTemplate =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 # https://docs.python.org/3.7/library/hashlib.html?highlight=blake#hashlib.blake2b
 # https://www.npmjs.com/package/blake2
@@ -97,27 +118,6 @@ def loadSponsorTestData() :
 
     client.close()
 
-animalImageIconType_choice = random.choice
-animalImageIconTypes = ['deskpro', 'docker', 'earlybirds', 'drupal', 'firefox',
-    'github', 'gitlab', 'grunt', 'linux', 'napster', 'phoenix framework', 'qq',
-    'reddit alien', 'snapchat ghost', 'sticker mule', 'twitter', 'tripadvisor',
-    'vaadin', 'themeisle', 'github alternate']
-
-animalCategoryType_choice = random.choice
-animalCategoryTypes = ['fishes', 'amphibians', 'reptiles', 'birds', 'mammals', 'invertebrates']
-
-endangeredTypes_choice = random.choice #Saves the choice([]) function for use later
-endangeredTypes = [ True, False]
-
-size_min = 100
-size_max = 500
-population_generator = random.randint #Saves the randint(min,max) function for use later
-
-description = '''Lorem ipsum dōlor sit ǽmet, reqūe tation constiÞuto vis eu, est ðōlor omnīum āntiopæm ei. 
-                Zril domīng cū eam, hās ið equīðem explīcærī voluptǽtum. Iusto regiōnē partiendo meǣ ne, vim 
-                cu ælii āltērum vōlutpāt, vis et aliquip trītæni. Dolor luptātum sapienÞem cu pēr, dico qūæs 
-                ðissentiǣs et eūm, vix ut.'''
-
 def bulkLoadAnimalTestData(insert_count: int = 100000) :
     print('Loading sample animal data')    
 
@@ -125,12 +125,12 @@ def bulkLoadAnimalTestData(insert_count: int = 100000) :
     db = client.get_database("rescueshelter")
 
     # db.drop_collection("animals")    
+    col = db.get_collection("animals")
 
     batches = bulkWriteListSizes(insert_count)
     for batch_size in batches :
         starttime = datetime.datetime.now()
-        col = db.get_collection("animals")
-
+        
         col.bulk_write([
             {
                 'insertOne': {            
@@ -151,66 +151,69 @@ def bulkLoadAnimalTestData(insert_count: int = 100000) :
                 } 
             }
             for i in range(batch_size)], ordered=False)            
-    
-        client.close()
-
+        
         endtime = datetime.datetime.now()
         print(f'Duration: {endtime-starttime}')
+    
+    client.close()
+
 
 def loadAnimalTestData(insert_count: int = 100000) :
     print('Loading sample animal data')
     starttime = datetime.datetime.now()
+
+    batches = bulkWriteListSizes(insert_count)
 
     client = pymongo.MongoClient("localhost", 27017)
     db = client.get_database("rescueshelter")
 
     # db.drop_collection("animals")
     col = db.get_collection("animals")
-    col.insert_many([
-        {
-            'name': ''.join(word_generator(wordTemplate, wordSize)),
-            'description': description,
-            'image': { 
-                'content': animalImageIconType_choice(animalImageIconTypes),
-                'contenttype': 'icon'
-            },
-            'category': animalCategoryType_choice(animalCategoryTypes),
-            'endangered': endangeredTypes_choice(endangeredTypes),
-            'data': populationData(population_generator(size_min,size_max)),
-            'dates': {
-                'created': datetime.datetime.utcnow(),
-                'modified': datetime.datetime.utcnow()
-            },
-            'sponsors': []
-        } for i in range(insert_count)])
+    for batch_size in batches :
+        col.insert_many([
+            {
+                'name': ''.join(word_generator(wordTemplate, wordSize)),
+                'description': description,
+                'image': { 
+                    'content': animalImageIconType_choice(animalImageIconTypes),
+                    'contenttype': 'icon'
+                },
+                'category': animalCategoryType_choice(animalCategoryTypes),
+                'endangered': endangeredTypes_choice(endangeredTypes),
+                'data': populationData(population_generator(size_min,size_max)),
+                'dates': {
+                    'created': datetime.datetime.utcnow(),
+                    'modified': datetime.datetime.utcnow()
+                },
+                'sponsors': []
+            } for i in range(batch_size)])
     
     client.close()
 
     endtime = datetime.datetime.now()
     print(f'Duration: {endtime-starttime}')
 
-def populationData(populationToday):
-    delta = datetime.timedelta(days=-270)
-    utcnow = datetime.datetime.utcnow()
-    population = populationToday
+def startMulticoreProcessFor(function, args: Iterable[Any], message  = 'Multiple core processor function evaluation') :
+    print(message)
+    starttime = datetime.datetime.now()
 
-    data = []
-    for i in range(100) :
-        if i >= 1 : # entry
-            utcnow = utcnow+delta 
-            population = random.randint(100, 200)
+    with Pool(processes=4) as pool :         
+        # launching multiple evaluations asynchronously *may* use more processes
+        asyncResults = [pool.apply_async(func=function, args=args) for i in range(4)]
+        
+        while True :
+            # Create a list of asyncResults ready state values
+            if False in [asyncResult.ready() for asyncResult in asyncResults] : continue # One function execution is not complete
+            else : break
 
-        data.append({
-            'population': population,
-            'created': utcnow
-        })
+    endtime = datetime.datetime.now()    
+    print(f'Duration: {endtime-starttime}')
 
-    return data
 
 def main() :
     loadSponsorTestData()
-    #loadAnimalTestData(100000)
-    startMulticoreProcessFor(bulkLoadAnimalTestData, [100000,], 'loading animal test data with bulk write')
+    loadAnimalTestData(100000)
+    #startMulticoreProcessFor(bulkLoadAnimalTestData, [100000,], 'loading animal test data with bulk write')
 
 if __name__ == "__main__":
     main()
